836dd1922630f24c9791abde8eb00bab63ead99d
Cameron Reimer <cjreimer@pecan-test.bu.edu>
2021-08-11 14:17:27 -0400

    much needed update

--- scripts/EFI_rerun.R
+++ scripts/EFI_rerun.R
@@ -19,7 +19,7 @@ args = list()
 args$settings = file.path(homedir, "/Site_XMLS/bart.xml") #remember to change to where you store the site.xmls
 args$continue = TRUE
 args$start_date = as.Date("2021-06-01") #remember to change start & end date to your desired dates
-args$end_date = as.Date("2021-06-12") #remember to change start & end date to your desired dates
+args$end_date = as.Date("2021-06-02") #remember to change start & end date to your desired dates
 
 args$outputPath <- file.path(homedir, "Site_Outputs/Bartlett/") #remember to change to where you want the model outputs saved
 
@@ -35,14 +35,13 @@ input_checkinfo <- list()
 # Open and read in settings file for PEcAn run.
 settings <- PEcAn.settings::read.settings(args$settings)
 site_id <- settings$run$site$id %>% as.character()
-#manually add in clim files 
 con <-try(PEcAn.DB::db.open(settings$database$bety), silent = TRUE)
 
 for(t in 1:length(dates)){
   
   input_check <- PEcAn.DB::dbfile.input.check(
                               siteid = site_id,
-                              startdate = dates[t],                #changed to loop through days here
+                              startdate = dates[t],     #changed to loop through days here
                               enddate = NULL,
                               parentid = NA,
                               mimetype="text/csv",
@@ -53,52 +52,34 @@ for(t in 1:length(dates)){
                               exact.dates = TRUE,
                               return.all=TRUE
                             )
+  
+  #Add successful input checks to the input_checkinfo object
   if(!is.null(input_check$id)){
-    input_checkinfo[[t]] = input_check
-    input_checkinfo[[t]]$date = dates[t]
     days_with_met_data = append(days_with_met_data, dates[t])
+    input_checkinfo[[length(days_with_met_data)]] = input_check
+    input_checkinfo[[length(days_with_met_data)]]$date = dates[t]
   }else{
       days_without_met_data = append(days_without_met_data, dates[t])
     }
 }
 
 
-
-
-#If INPUTS already exists, add id and met path to settings file
-
-if(length(input_check$id) > 0){
-  #met paths 
-  clim_check = list()
-  for(i in 1:length(input_check$file_path)){
-    
-    clim_check[[i]] <- file.path(input_check$file_path[i], input_check$file_name[i])
-  }#end i loop for creating file paths 
-  #ids
-  index_id = list()
-  index_path = list()
-  for(i in 1:length(input_check$id)){
-    index_id[[i]] = as.character(input_check$id[i])#get ids as list
-    
-  }#end i loop for making lists
-  names(index_id) = sprintf("id%s",seq(1:length(input_check$id))) #rename list
-  names(clim_check) = sprintf("path%s",seq(1:length(input_check$id)))
-  
-  settings[[t]]$run$inputs$met$id = index_id
-  settings[[t]]$run$inputs$met$path = clim_check
-}else{PEcAn.utils::logger.error("No met file found")}
-#settings <- PEcAn.workflow::do_conversions(settings, T, T, T)
-
-if(is_empty(settings[[t]]$run$inputs$met$path) & length(clim_check)>0){
-  settings[[t]]$run$inputs$met$id = index_id
-  settings[[t]]$run$inputs$met$path = clim_check
+#if there are NO met files for any of the days, throw an error
+if(length(days_without_met_data) == length(dates)){
+  PEcAn.utils::logger.error("No met files found")
 }
-PEcAn.DB::db.close(con)
 
-# Write out the file with updated settings
-PEcAn.settings::write.settings(settings[[t]], outputfile = "pecan.GEFS.xml")
- 
-print(days_with_met_data)
+#format EFI_workflow system call 
+source("/projectnb/dietzelab/cjreimer/pecan/scripts/EFI_workflow.R")
+site_path <- args$settings
+output_path <- args$outputPath
+
+for(i in 1:length(days_with_met_data)){
+  start_date = as.character(days_with_met_data[i])
+  system2(command = "Rscript --vanilla EFI_workflow.R", args = c(site_path, output_path, start_date))
+}
+
+
 
 
 
